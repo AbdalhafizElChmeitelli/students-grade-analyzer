49a060970ad34a067a410fb0a1eeb7b16b289b1b
Abd al Hafiz El-Chmeitelli <abdulhafeez.alshmaitelly@gmail.com>
2025-06-09 23:25:17 +0300

    adding the function get_avg_grade

--- students-grade-analyzer.py
+++ students-grade-analyzer.py
@@ -16,4 +16,13 @@ def display_student_summary():
         
      print(student[j] + ": " + str(grades[j]))
 
+def get_avg_grade(grades, num_students):
+    x = 0
+    for i in range(num_students):
+        x += grades[i]
+    y = x / num_students if num_students > 0 else 0
+    print("Average grade: " + str(y))
+
+
 display_student_summary()
+get_avg_grade(grades, num_students)
